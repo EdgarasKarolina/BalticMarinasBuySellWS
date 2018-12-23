d131515e1d4cb513ff57487c66eaf4c99136c504
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2018-12-23 17:17:12 +0100

    More readable querries

--- BalticMarinasBuySellWS/Utilities/Queries.cs
+++ BalticMarinasBuySellWS/Utilities/Queries.cs
@@ -2,13 +2,13 @@
 {
     public static class Queries
     {
-        public const string GetAllSoldItems = "select * from sold_items";
+        public const string GetAllSoldItems = "SELECT * FROM sold_items";
 
-        public const string GetAllSoldItemsByUserId = "select * from sold_items where UserId = @userId";
+        public const string GetAllSoldItemsByUserId = "SELECT * FROM sold_items WHERE UserId = @userId";
 
-        public const string GetSoldItemById = "select * from sold_items where SoldItemId = @id";
+        public const string GetSoldItemById = "SELECT * FROM sold_items WHERE SoldItemId = @id";
 
-        public const string DeleteSoldItemById = "delete from sold_items where SoldItemId = @id";
+        public const string DeleteSoldItemById = "DELETE FROM sold_items WHERE SoldItemId = @id";
 
         public const string CreateSoldItem = "INSERT INTO sold_items (Title, Category, Price, MadeYear, Description, UserId)\n" +
                     "VALUES (@title, @category, @price, @madeYear, @description, @userId);";
